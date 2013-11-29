35375ae71e677bbfcb4a5661107d14092880085a
Hubert Figuière <hub@figuiere.net>
2013-11-29 01:06:24 -0500

    Bump to 2.1.0

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 /* Version information for exifprobe                                  */
 /* @(#) $Id: version.h,v 1.17 2005/07/24 23:07:57 alex Exp $ */
 
-char *Copyright = "Copyright (C) 2005 Duane H. Hesser, (C) 2011 Hubert Figuiere";
-char *Program_version = "2.0.2";
+char *Copyright = "Copyright (C) 2005 Duane H. Hesser, (C) 2011-2013 Hubert Figuiere";
+char *Program_version = "2.1.0";
 #define PATCHLEVEL    0
